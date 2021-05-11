bf5665f4c364d7bd64298bfefbcb5fb231440ce1
marcioTomaz <marciotmaz@gmail.com>
2021-05-11 13:12:49 -0300

    add: botao, linha e hierarquia

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -11,9 +12,29 @@ void main() {
             "Pessoas: 0",
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "-1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
           Text(
             "Pode entrar",
-            style: TextStyle(color: Colors.green,
+            style: TextStyle(
+                color: Colors.green,
                 fontStyle: FontStyle.italic,
                 fontSize: 30.0),
           )
